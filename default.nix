eba4e71bd5127a5c7a3e2e4e722d3959ee607795
mekosko <mekosko@morojenoye.com>
2024-08-18 00:07:38 +0500

    Setup nix build

--- default.nix
+++ default.nix
@@ -1,6 +1,20 @@
-{ pkgs ? import <nixpkgs> { crossSystem = "riscv64-linux"; } }:
+{ pkgs ? import <nixpkgs> { } }:
 
-pkgs.symlinkJoin {
-  name = "morojenoye";
-  paths = [ pkgs.ubootQemuRiscv64Smode ];
-}
+pkgs.linkFarm "morojenoye" [
+  {
+    name = "riscv64";
+    path = pkgs.symlinkJoin {
+      name = "morojenoye-riscv64";
+      paths = let pkgs = import <nixpkgs> { crossSystem = "riscv64-linux"; };
+      in [ pkgs.ubootQemuRiscv64Smode ];
+    };
+  }
+  {
+    name = "aarch64";
+    path = pkgs.symlinkJoin {
+      name = "morojenoye-aarch64";
+      paths = let pkgs = import <nixpkgs> { crossSystem = "aarch64-linux"; };
+      in [ pkgs.ubootQemuAarch64 ];
+    };
+  }
+]
